70a57f0667d060a31ff70e380a84768276d122de
Clarian <48294536+clarianx@users.noreply.github.com>
2022-04-06 14:45:50 +0200

    Update clarian.py

--- clarian.py
+++ clarian.py
@@ -7,7 +7,7 @@ print("======================")
 print("Il bot si sta avviando...")
 
 def get_prefix(bot, message):
-    with open('json\prefixes.json', 'r') as f:
+    with open('json/prefixes.json', 'r') as f:
         prefixes = json.load(f)
     return prefixes[str(message.guild.id)]
 
@@ -28,27 +28,27 @@ async def on_ready():
 
 @bot.event
 async def on_guild_join(guild):
-    with open('json\prefixes.json', 'r') as f:
+    with open('json/prefixes.json', 'r') as f:
         prefixes = json.load(f)
     prefixes[str(guild.id)] = '.'
-    with open('json\prefixes.json', 'w') as f:
+    with open('json/prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
 
 @bot.event
 async def on_guild_remove(guild):
-    with open('json\prefixes.json', 'r') as f:
+    with open('json/prefixes.json', 'r') as f:
         prefixes = json.load(f)
     prefixes.pop(str(guild.id))
-    with open('json\prefixes.json', 'w') as f:
+    with open('json/prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
 
 @bot.slash_command(name='changeprefix', description='Change my command prefix', pass_context=True)
 @commands.has_permissions(administrator=True)
 async def changeprefix(ctx, prefix):
-    with open('json\prefixes.json', 'r') as f:
+    with open('json/prefixes.json', 'r') as f:
         prefixes = json.load(f)
     prefixes[str(ctx.guild.id)] = prefix
-    with open('json\prefixes.json', 'w') as f:
+    with open('json/prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
     embed1=discord.Embed(title="New Prefix", color=0x14f910)
     embed1.add_field(name="My new prefix is:", value=f"{prefix}", inline=True)
